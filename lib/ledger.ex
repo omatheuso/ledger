e1b601a0f98c2411fdc9140623f6c181c8de65de
Matheus de Oliveira <matheuscdeoliveira@gmail.com>
2023-04-24 19:12:38 -0700

    challenge started

---
+++ lib/ledger.ex
@@ -0,0 +1,18 @@
+defmodule Ledger do
+  @moduledoc """
+  Documentation for `Ledger`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ledger.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
